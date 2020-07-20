886947498fa93def4beaa1427ff8563038bc7faf
Angelo Carvalho <angelosuporteti@gmail.com>
2020-07-20 12:49:08 -0300

    Implementação da classe ContaEMpresarial

---
+++ HerancaPolimorfismo/Entidades/ContaEmpresarial.cs
@@ -0,0 +1,26 @@
+﻿namespace HerancaPolimorfismo.Entidades
+{
+    class ContaEmpresarial : Conta // --> : <-- Herança das propriedades e métodos da clase Conta
+    {
+        public double LimiteEmprestimo { get; set; }
+
+        public ContaEmpresarial()
+        {
+        }
+
+        public ContaEmpresarial(int numero, string titular, double saldo, double limiteEmprestimo) 
+            : base (numero, titular, saldo) //Reutilização de parâmetros da superclasse
+        {
+            LimiteEmprestimo = limiteEmprestimo;
+        }
+
+        public void Emprestimo(double quantia)
+        {
+            if (quantia <= LimiteEmprestimo){
+                Saldo += quantia;
+            }
+        }
+
+
+    }
+}

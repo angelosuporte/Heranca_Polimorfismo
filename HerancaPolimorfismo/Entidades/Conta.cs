4f7befa7dba150bf3f9a85e68e9a9e8294864233
Angelo Carvalho <angelosuporteti@gmail.com>
2020-07-20 12:27:23 -0300

    Aprendizado em Herança e Polimorfismo - Implementação da classe Conta

---
+++ HerancaPolimorfismo/Entidades/Conta.cs
@@ -0,0 +1,30 @@
+﻿namespace HerancaPolimorfismo.Entidades
+{
+    class Conta
+    {
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+
+        public Conta()
+        {
+        }
+
+        public Conta(int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia;
+        }
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+    }
+    
+}
---
+++ HerancaPolimorfismo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HerancaPolimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
